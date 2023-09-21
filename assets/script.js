f51d132ad1e76aeece57e3297e339661abfa9b44
Manav Jawrani <manavjawrani7@gmail.com>
2023-09-22 03:36:31 +0530

    Added CSS

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,50 @@
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+const tableBody = document.getElementById('repo-table');
+const fetchButton = document.getElementById('fetch-button');
 
 // function getApi() {
 //     // fetch request gets a list of all the repos for the node.js organization
-//     var requestUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
-  
+//     const requestUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
 //     fetch(requestUrl)
-//       .then(function (response) { //first response 
+//       .then(function (response) {
 //         return response.json();
 //       })
-//       .then(function (data) { //2nd  data
-//         console.log(data)
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        // for (var i = 0; i < data.length; i++) {
-        //   // Creating elements, tablerow, tabledata, and anchor
-        //   var createTableRow = document.createElement('tr');
-        //   var tableData = document.createElement('td');
-        //   var link = document.createElement('a');
-  
-        //   // Setting the text of link and the href of the link
-        //   link.textContent = data[i].html_url;
-        //   link.href = data[i].html_url;
-  
-        //   // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        //   // The tablerow then gets appended to the tablebody
-        //   tableData.appendChild(link);
-        //   createTableRow.appendChild(tableData);
-        //   tableBody.appendChild(createTableRow);
-        //   console.log(fetchButton)
-        // }
-    //  });
-  // }
+//       .then(function (data) {
+//         console.log(data);
 
-  // getApi();
-  
-  // fetchButton.addEventListener('click', getApi);
+//         for (let i = 0; i < data.length; i++) {
+//           const createTableRow = document.createElement('tr');
+//           const tableData = document.createElement('td');
+//           const link = document.createElement('a');
 
-  
-  const form = document.querySelector('form');
-  // form.submit();
+//           link.textContent = data[i].html_url;
+//           link.href = data[i].html_url;
 
-  //Matt js
-  var submitButton = document.querySelector('#submitBtn');
-var userSkinTone = document.querySelector('#userTone');
-var makeupForm = document.querySelector('#userMakeup');
-var resetButton = document.querySelector('#resetBtn');
+//           tableData.appendChild(link);
+//           createTableRow.appendChild(tableData);
+//           tableBody.appendChild(createTableRow);
+//         }
+//       });
+// }
 
+// getApi();
+
+// fetchButton.addEventListener('click', getApi);
+
+const form = document.querySelector('form');
+
+const submitButton = document.querySelector('#submitBtn');
+const userSkinTone = document.querySelector('#userTone');
+const makeupForm = document.querySelector('#userMakeup');
+const resetButton = document.querySelector('#resetBtn');
 
 const umakeupForm = document.getElementById("userMakeup");
 const skinToneInput = document.getElementById("userSkinTone");
 const lipstickInput = document.getElementById("userLipstick");
 const eyeshadowInput = document.getElementById("userEyeshadow");
+const mascaraInput = document.getElementById("userMascara");
 const makeupSubmit = document.getElementById("submitBtn");
 const userList = document.getElementById("userList");
-
-// function saveMakeup(){
-  
-// var makeupUser = {
-//   userTone: userTone.value
- 
-// };
-
-// localStorage.setItem('makeupUser', JSON.stringify(makeupUser));
-//   };
-
-// function displayMakeup(e){
-//   console.log(e.target);
-// e.preventDefault();
-// console.log(e);
-// var userSelect =JSON.parse(localStorage.getItem("makeupUser"));
-// console.log(userSelect.userTone);
-// }
-
-// console.log(userSelect);
-
-
-
-//submitButton.addEventListener("click", displayMakeup);//{
-//   e.preventDefault();
-//   saveMakeup();
-//   displayMakeup();
-// });
 
 let userStorage = localStorage.getItem("userList")
   ? JSON.parse(localStorage.getItem("userList"))
@@ -91,17 +55,17 @@ umakeupForm.addEventListener("submit", (e) => {
   userStorage.push(skinToneInput.value);
   userStorage.push(lipstickInput.value);
   userStorage.push(eyeshadowInput.value);
-  //notesStorage.push(noteInput.value);
+  userStorage.push(mascaraInput.value);
   localStorage.setItem("userList", JSON.stringify(userStorage));
   listBuilder(skinToneInput.value);
   listBuilder(lipstickInput.value);
   listBuilder(eyeshadowInput.value);
-  //noteInput.value = "";
+  listBuilder(mascaraInput.value);
 });
 
 const listBuilder = (text) => {
   const item = document.createElement("li");
-  item.innerHTML = text + ' <button onclick="deleteNote(this)">x</button>';
+  item.innerHTML = text;
   userList.appendChild(item);
 };
 
@@ -110,30 +74,7 @@ getChoices.forEach((item) => {
   listBuilder(item);
 });
 
-// resetButton.addEventListener('click', 
-// notes.innerHTML="";
-// localStorage.clear();
-
-
 resetButton.addEventListener('click', function(){
-localStorage.clear();
-window.location.reload();
+  localStorage.clear();
+  window.location.reload();
 });
-
-
-// localStorage.setItem("makeupUser", JSON.stringify(makeupUser));
-// renderMessage();
-
-// // });
-
-// function renderMessage() {
-// var user = JSON.parse(localStorage.getItem("makeupUser"));
-
-// console.log(user);
-//   // document.getElementById("test").textContent = lastCx.customer + 
-//   // " received a/an "
-// }
-
-
-//   let userSkinTone = document.getElementById('skinTone').value;
-
