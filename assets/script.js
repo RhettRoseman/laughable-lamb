00ddaf5268e04833aca9ea7361cacf40ecec99f7
RhettRoseman <rroseman@rcplanners.com>
2023-09-22 14:29:36 -0400

    added full js

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,46 @@
 //Created by MB 
 
 var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+var fetchButton = document.getElementById('submitBtn');
 
+
+// vars for fetch display @MEB
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+
+
+
+
+//var fetchButton = document.getElementById('fetch-button');
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
-  
+    var requestUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+ 
     fetch(requestUrl)
-      .then(function (response) { //first response 
+      .then(function (response) { //first response
         return response.json();
+
+
+        //display the recommended makeup type based on the weather @MEB
+      //   if(weatherForecast === sunny){
+      //     //wear makeup type a
+      //   }else if(weatherForecast === rainy){
+      //     //wear makeup b
+      //   } else //wear makeup c
       })
       .then(function (data) { //2nd  data
         console.log(data)
         // Loop over the data to generate a table, each table row will have a link to the repo url
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < 10; i++) {
           // Creating elements, tablerow, tabledata, and anchor
-          var createTableRow = document;
+          var createTableRow = document.createElement('tr');
           var tableData = document.createElement('td');
           var link = document.createElement('a');
-  
+ 
           // Setting the text of link and the href of the link
           link.textContent = data[i].html_url;
           link.href = data[i].html_url;
-  
+ 
           // Appending the link to the tabledata and then appending the tabledata to the tablerow
           // The tablerow then gets appended to the tablebody
           tableData.appendChild(link);
@@ -34,13 +51,16 @@ function getApi() {
      });
   }
 
+
   getApi();
-  
+ 
   fetchButton.addEventListener('click', getApi);
 
-  
+
+ 
   const form = document.querySelector('form');
   // form.submit();
+
 
   //Matt js
   var submitButton = document.querySelector('#submitBtn');
@@ -49,22 +69,29 @@ var makeupForm = document.querySelector('#userMakeup');
 var resetButton = document.querySelector('#resetBtn');
 
 
+
+
 const umakeupForm = document.getElementById("userMakeup");
 const skinToneInput = document.getElementById("userSkinTone");
 const lipstickInput = document.getElementById("userLipstick");
 const eyeshadowInput = document.getElementById("userEyeshadow");
+const mascaraInput = document.getElementById("userMascara");
+const featuresInput = document.getElementById("userFeatures");
 const makeupSubmit = document.getElementById("submitBtn");
 const userList = document.getElementById("userList");
 
+
 // function saveMakeup(){
-  
+ 
 // var makeupUser = {
 //   userTone: userTone.value
  
 // };
 
+
 // localStorage.setItem('makeupUser', JSON.stringify(makeupUser));
 //   };
+
 
 // function displayMakeup(e){
 //   console.log(e.target);
@@ -74,7 +101,11 @@ const userList = document.getElementById("userList");
 // console.log(userSelect.userTone);
 // }
 
+
 // console.log(userSelect);
+
+
+
 
 
 
@@ -84,34 +115,48 @@ const userList = document.getElementById("userList");
 //   displayMakeup();
 // });
 
+
 let userStorage = localStorage.getItem("userList")
   ? JSON.parse(localStorage.getItem("userList"))
   : [];
-//results page MB
+
+
 umakeupForm.addEventListener("submit", (e) => {
   e.preventDefault();
   userStorage.push(skinToneInput.value);
   userStorage.push(lipstickInput.value);
   userStorage.push(eyeshadowInput.value);
-
+  userStorage.push(mascaraInput.value);
+  userStorage.push(featuresInput.value);
   //notesStorage.push(noteInput.value);
   localStorage.setItem("userList", JSON.stringify(userStorage));
   listBuilder(skinToneInput.value);
   listBuilder(lipstickInput.value);
   listBuilder(eyeshadowInput.value);
+  listBuilder(mascaraInput.value);
+  listBuilder(featuresInput.value);
   //noteInput.value = "";
 });
+
 
 const listBuilder = (text) => {
   const item = document.createElement("li");
   item.innerHTML = text + ' <button onclick="deleteNote(this)">x</button>';
   userList.appendChild(item);
 };
-//commented 
 
-// resetButton.addEventListener('click', 
+
+const getChoices = JSON.parse(localStorage.getItem("userList"));
+getChoices.forEach((item) => {
+  listBuilder(item);
+});
+
+
+// resetButton.addEventListener('click',
 // notes.innerHTML="";
 // localStorage.clear();
+
+
 
 
 resetButton.addEventListener('click', function(){
@@ -120,19 +165,48 @@ window.location.reload();
 });
 
 
+
+
 // localStorage.setItem("makeupUser", JSON.stringify(makeupUser));
 // renderMessage();
 
+
 // // });
+
 
 // function renderMessage() {
 // var user = JSON.parse(localStorage.getItem("makeupUser"));
 
+
 // console.log(user);
-//   // document.getElementById("test").textContent = lastCx.customer + 
+//   // document.getElementById("test").textContent = lastCx.customer +
 //   // " received a/an "
 // }
 
 
+
+
 //   let userSkinTone = document.getElementById('skinTone').value;
 
+
+
+
+
+
+//  Handle @MEB
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+
+  searchApi(searchInputVal, formatInputVal);
+}
